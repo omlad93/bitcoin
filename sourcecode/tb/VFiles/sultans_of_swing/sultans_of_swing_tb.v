d8b161e34255789252acf4691c78206058521a14
Omri Elad <omlad93@gmail.com>
2022-04-29 16:15:32 +0300

    Make Sultan Great Again

--- sourcecode/tb/VFiles/sultans_of_swing/sultans_of_swing_tb.v
+++ sourcecode/tb/VFiles/sultans_of_swing/sultans_of_swing_tb.v
@@ -17,9 +17,7 @@ module sultans_of_swing_tb;
     reg [3:0] B_e;        //Expected Result
     reg [3:0] AND_e;      //Expected Result
 
-// our clock period
-// this is just an example for a period, insignificant at the moment.
-	always #5 clk = ~clk; 
+
 
 
 // Expected Behavioural:
@@ -41,36 +39,58 @@ module sultans_of_swing_tb;
                             .ANDo(AND_out)
     );
 
+    // our clock period
+    // this is just an example for a period, insignificant at the moment.
+	always #1 clk = ~clk; 
+    always @(posedge clk) begin : expected
+        if (reset) begin
+            A_e <= 4'b0; 
+            B_e <= 4'b0; 
+            AND_e <= 4'b0; 
+        end else begin
+            A_e <= A_in;
+            B_e <= B_in;
+            AND_e <= (A_in ^ B_in) & (C_in);
+        end
+        $display("| $time | A   out |   %b   |   %b   |  %b  |",A_e,A_out,A_e==A_out);
+        $display("| $time | B   out |   %b   |   %b   |  %b  |",B_e,B_out, B_e==B_out);
+        $display("| $time | AND out |   %b   |   %b   |  %b  |",AND_e,AND_out, AND_e==AND_out);
+
+    end
 
     initial begin
-
+        $display("| Time |  Signal  | Expected | Computed | V |");
         A_in    = 4'b1111;
         B_in    = 4'b1111;
         C_in    = 4'b1111;
         reset   = 1'b1;
-        #10
+        #2
         reset = 1'b0;
-        #10
+        #2.5
+
         A_in    = 4'b0000;
         B_in    = 4'b0000;
         C_in    = 4'b0000;
-        #10
-        A_e     = 4'b0000;        //Expected Result
-        B_e     = 4'b0000;        //Expected Result
-        AND_e   = 4'b0000;      //Expected Result
-        #10
+        #5.5
+
         A_in    = 4'b0011;
         B_in    = 4'b0110;
         C_in    = 4'b0001;
-        #10
-        A_e     = 4'b0011;        //Expected Result
-        B_e     = 4'b0110;        //Expected Result
-        AND_e   = 4'b0001;      //Expected Result
-        #10
+
+        A_in    = 4'b1111;
+        B_in    = 4'b0000;
+        C_in    = 4'b0110;
+        #5.5
+        reset = 1'b1;
+
         /////////////
 
         $finish;
     end
+
     
+    
+
+
 
 endmodule
