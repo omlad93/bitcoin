bf3792c950042ae2d8d28d6485bad3d6ae20bd04
Ido Datner <datner.ido@gmail.com>
2022-05-09 17:12:50 +0300

    wembley_88 most updated changes

---
+++ sourcecode/rtl/VFiles/wembley_88/wembley_88.v
@@ -0,0 +1,24 @@
+`timescale 1ns/100ps
+
+module wembley_88(
+    input wire [3:0] Ain, Bin, Cin,              
+    input wire clk,
+    input wire reset,
+    output wire Yout
+    );
+
+    // Wire Declerations
+    wire [3:0] A_out;
+    wire [3:0] B_out;
+    wire [3:0] AND_out;
+    wire [3:0] A_e;
+    wire [3:0] B_e;
+    wire [3:0] C_e;
+
+    sultans_of_swing(.Ai(Ain), .Bi(Bin), .Ci(Cin), .Ao(A_out), .clk(clk), .reset(reset), .Bo(B_out), .ANDo(AND_out));
+    dire_straits(.A_out(A_out), .B_out(B_out), .AND_out(AND_out), .A_e(A_e), .B_e(B_e), .C_e(C_e));
+    eric_clapton(.A_e(A_e), .B_e(B_e), .C_e(C_e), .clk(clk), .reset(reset), .Yout(Yout));
+
+
+
+endmodule
