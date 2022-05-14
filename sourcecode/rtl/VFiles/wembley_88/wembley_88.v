eb3ad8dca70a97bb327fa20cea02e8217fb223dd
Omri Elad <omlad93@gmail.com>
2022-05-14 12:53:13 +0300

    makefile and more

--- sourcecode/rtl/VFiles/wembley_88/wembley_88.v
+++ sourcecode/rtl/VFiles/wembley_88/wembley_88.v
@@ -15,9 +15,9 @@ module wembley_88(
     wire [3:0] B_e;
     wire [3:0] C_e;
 
-    sultans_of_swing(.Ai(Ain), .Bi(Bin), .Ci(Cin), .Ao(A_out), .clk(clk), .reset(reset), .Bo(B_out), .ANDo(AND_out));
-    dire_straits(.A_out(A_out), .B_out(B_out), .AND_out(AND_out), .A_e(A_e), .B_e(B_e), .C_e(C_e));
-    eric_clapton(.A_e(A_e), .B_e(B_e), .C_e(C_e), .clk(clk), .reset(reset), .Yout(Yout));
+    sultans_of_swing sultan(.Ai(Ain), .Bi(Bin), .Ci(Cin), .Ao(A_out), .clk(clk), .reset(reset), .Bo(B_out), .ANDo(AND_out));
+    dire_straits dire(.A_out(A_out), .B_out(B_out), .AND_out(AND_out), .A_e(A_e), .B_e(B_e), .C_e(C_e));
+    eric_clapton eric(.A_e(A_e), .B_e(B_e), .C_e(C_e), .clk(clk), .reset(reset), .Yout(Yout));
 
 
 
